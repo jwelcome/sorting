67e53132def5294bfcfb4afc55fc41f057756e7f
Juliet Welcome <juliet.welcome@gmail.com>
2018-07-30 11:43:17 -0500

    swap function

--- bubblesort.js
+++ bubblesort.js
@@ -1,9 +1,11 @@
+let counter = 0;
+
 function swap(arr) {
-  if (!arr.length) {
+  if (arr.length === counter) {
     return arr;
   } else {
     //compare two elements
-    for (var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length - counter; i++) {
       console.log(arr[i]);
       if (arr[i] > arr[i+1]) {
         let swappedItem = arr[i+1];
@@ -12,14 +14,14 @@ function swap(arr) {
         console.log(arr);
       }
     }
+
+    counter++;
+    console.log('number is', counter);
+
     //recurse
-    return swap(arr.slice(0, arr.length - 1));
+    return swap(arr);
   }
 }
-
-
-
-
 
 function bubbleSort(arr) {
   return swap(arr);
